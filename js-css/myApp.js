bc15d025234c5348abc3b5a58e084159c7276889
phpusr <phpusr@gmail.com>
2013-11-13 22:36:18 +0600

    Добавил вывод заголовков и подсчетов

--- js-css/myApp.js
+++ js-css/myApp.js
@@ -4,13 +4,29 @@ app.controller('MyCtrl', function MyCtrl($scope) {
     $scope.changeParams = function() {
 
         if ($scope.rows != null && $scope.columns != null) {
+            //Вероятность (события равновероятны)
+            $scope.p = 1/$scope.rows;
 
+            //Заголовки
+            $scope.rows++;
+            $scope.columns++;
+
+            //Заполнение заголовков
             $scope.table = [];
-
-            for (var i=0; i<$scope.rows; i++) {
+            $scope.table[0] = [];
+            $scope.table[0][0] = new Data('X', true);
+            for (var j=1; j<$scope.columns; j++) {
+                $scope.table[0][j] = new Data('Param '+j, true);
+            }
+            for (i=1; i<$scope.rows; i++) {
                 $scope.table[i] = [];
-                for (var j=0; j<$scope.columns; j++) {
-                    $scope.table[i][j] = new Data(0);
+                $scope.table[i][0] = new Data(i, true);
+            }
+
+            //Заполнение нулями
+            for (var i=1; i<$scope.rows; i++) {
+                for (j=1; j<$scope.columns; j++) {
+                    $scope.table[i][j] = new Data(0, false);
                 }
             }
         }
@@ -19,23 +35,29 @@ app.controller('MyCtrl', function MyCtrl($scope) {
     $scope.changeTable = function() {
         //console.log('table', $scope.table);
 
-        $scope.sum = 0;
         $scope.sums = [];
 
-        for (var i=0; i<$scope.rows; i++) {
-            $scope.sums[i] = 0;
-            for (var j=0; j<$scope.columns; j++) {
-                $scope.sum += parseInt($scope.table[i][j].val);
-                $scope.sums[i] += parseInt($scope.table[i][j].val);
+        for (var i=1; i<$scope.rows; i++) {
+            var rowSum = 0;
+            $scope.sums[i] = '(';
+            for (var j=1; j<$scope.columns; j++) {
+                var val = parseInt($scope.table[i][j].val);
+                rowSum += val;
+                $scope.sums[i] += val;
+                if (j < $scope.columns-1) $scope.sums[i] += ' + '
             }
+            $scope.sums[i] += ') * ' + $scope.p + ' = ' + rowSum*$scope.p;
         }
     };
 
     //Начальная инициализация
-    $scope.rows = 5;
-    $scope.columns = 6;
+    $scope.rows = 4;
+    $scope.columns = 4;
     $scope.changeParams();
     $scope.changeTable();
 });
 /** Структура для хранения данных ячейки */
-function Data(val) { this.val = val }
+function Data(val, title) {
+    this.val = val;
+    this.title = title;
+}
