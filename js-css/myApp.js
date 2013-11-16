5a738b07596fd1857785288499c88d83007d7cd3
phpusr <phpusr@gmail.com>
2013-11-16 15:26:01 +0600

    Добавил функцию заполнения данными, заполнение значениями по умолчанию

--- js-css/myApp.js
+++ js-css/myApp.js
@@ -20,13 +20,13 @@ app.controller('MyCtrl', function MyCtrl($scope) {
             for (var j=1; j<columns; j++) {
                 $scope.table[0][j] = new Data('Param '+j, true);
             }
-            for (i=1; i<rows; i++) {
+            for (var i=1; i<rows; i++) {
                 $scope.table[i] = [];
                 $scope.table[i][0] = new Data(i, true);
             }
 
             //Заполнение нулями
-            for (var i=1; i<rows; i++) {
+            for (i=1; i<rows; i++) {
                 for (j=1; j<columns; j++) {
                     $scope.table[i][j] = new Data(0, false);
                 }
@@ -55,10 +55,32 @@ app.controller('MyCtrl', function MyCtrl($scope) {
         }
     };
 
+    /** Заполнение значениями по умолчанию */
+    $scope.stdFill = function() {
+        $scope.rows = 4;
+        $scope.columns = 4;
+        $scope.changeParams();
+        loadData([
+            50,100,180,250,
+            80,70,80,230,
+            40,180,120,210,
+            300,220,150,150
+        ]);
+        $scope.changeTable();
+    };
+
+    /** Загрузка данных в таблицу */
+    function loadData(data) {
+        for (var i=1; i<rows; i++) {
+            for (var j=1; j<columns; j++) {
+                var val = data[(i-1)*(columns-1)+(j-1)];
+                $scope.table[i][j] = new Data(val, false);
+            }
+        }
+    }
+
     //Начальная инициализация
-    $scope.rows = 4;
-    $scope.columns = 4;
-    $scope.changeParams();
+    $scope.stdFill();
 });
 /** Структура для хранения данных ячейки */
 function Data(val, title) {
